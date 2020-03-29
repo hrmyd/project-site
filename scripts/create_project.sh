a952ee742681a7e6387d4ba09fb72ad681245e77
Hiranmayi Duvvuri <hiranmayid8@gmail.com>
2020-03-29 16:00:33 -0700

    this is an unhelpful commit message

--- scripts/create_project.sh
+++ scripts/create_project.sh
@@ -1,12 +1,27 @@
+#!/bin/bash
 
+PROJECT_NAME=$1
+BUCKET_NAME="$1-data"
+SUBDOMAIN_NAME=$2
 
+cd .
 
+mkdir $PROJECT_NAME/
+cd $PROJECT_NAME/
 
+cp requirements.txt $PROJECT_NAME/requirements.txt
 
-# create folder for service
-# create app.yaml
-# create sub-domain
-# create bucket
-# add to dispatch.yaml
+echo "runtime: custom
+env: flex
+service: $PROJECT_NAME
 
-# deploy -> deploy app, deploy dispatch.yaml
+env_variables:
+    BUCKET_NAME: $BUCKET_NAME" > app.yaml
+
+echo "FROM python:3.7-slim
+
+# Copy local code to the container image.
+WORKDIR /app
+COPY . ./" > Dockerfile
+
+gsutil mb gs://$BUCKET_NAME/
